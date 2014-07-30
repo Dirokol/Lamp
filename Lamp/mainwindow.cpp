5db13ced6f66b5667d9a435d840a46ed8fbdb0c6
a.myachin <myachin@rospartner.ru>
2014-07-30 22:08:59 +0400

    Прописано подключение по Tcp протоколу

--- Lamp/mainwindow.cpp
+++ Lamp/mainwindow.cpp
@@ -1,3 +1,5 @@
+#include <QAbstractSocket>
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -11,12 +13,20 @@ MainWindow::MainWindow(QWidget *parent) :
 
     this->setWindowTitle("Lamp");
 
+    m_hostname = "";
+    m_port = 0;
+
     AuthWin win(this);
 
     connect(&win, SIGNAL(hostname(QString)), this, SLOT(setHostName(QString)));
     connect(&win, SIGNAL(port(u_int16_t)), this, SLOT(setPort(u_int16_t)));
 
     win.exec();
+
+    if (!connecting())
+        exit(EXIT_FAILURE);
+
+    connect(m_tcp, SIGNAL(readyRead()), this, SLOT(on_recv_server()));
 }
 
 MainWindow::~MainWindow()
@@ -26,6 +36,67 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_m_exit_button_released()
 {
-    // TODO Записать завершение подключения
+    disconnecting();
     exit(EXIT_SUCCESS);
+}
+
+bool MainWindow::connecting()
+{
+    m_tcp = new QTcpSocket(this);
+
+    m_tcp->connectToHost(m_hostname, m_port);
+    m_tcp->waitForConnected();
+
+    if (m_tcp->state() != QAbstractSocket::ConnectedState)
+    {
+        delete m_tcp;
+        m_tcp = NULL;
+
+        return false;
+    }
+
+    return true;
+}
+
+void MainWindow::disconnecting()
+{
+    static char cmd_bye[] = "bye";
+
+    send_to_server(cmd_bye, strlen(cmd_bye));
+
+    if (m_tcp && m_tcp->state() != QAbstractSocket::UnconnectedState)
+    {
+        m_tcp->disconnect();
+        m_tcp->waitForDisconnected();
+    }
+    delete m_tcp;
+    m_tcp = NULL;
+}
+
+bool MainWindow::send_to_server(const void *buf, size_t sz)
+{
+    if (m_tcp && m_tcp->state() == QAbstractSocket::ConnectedState)
+    {
+        size_t len = sz;
+        char *dst = (char *)buf;
+
+        while (len > 0)
+        {
+            ssize_t sent = m_tcp->write(dst, len);
+            m_tcp->flush();
+
+            if (sent < 0)
+                return false;
+
+            len -= sent;
+            dst += sent;
+        }
+        return true;
+    }
+    return false;
+}
+
+void MainWindow::on_recv_server()
+{
+
 }
--- Lamp/mainwindow.h
+++ Lamp/mainwindow.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 #include <QMainWindow>
+#include <QtNetwork/QTcpSocket>
 
 namespace Ui {
 class MainWindow;
@@ -21,11 +22,21 @@ public slots:
     void setHostName(QString hostname) { m_hostname = hostname; }
     void setPort(u_int16_t port) { m_port = port; }
 
+private slots:
+    void on_m_exit_button_released();
+    void on_recv_server();
+    
 private:
+    bool connecting();
+    void disconnecting();
+    bool send_to_server(const void *buf, size_t sz);
+
     Ui::MainWindow *ui;
 
     QString     m_hostname;
     u_int16_t   m_port;
+
+    QTcpSocket  *m_tcp;
 };
 
 #endif // MAINWINDOW_H
